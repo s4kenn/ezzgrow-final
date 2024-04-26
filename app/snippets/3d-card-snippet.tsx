8423b627b6c8c5e20767686df854a2e92a667a43
Aditya Singh <118333811+s4kenn@users.noreply.github.com>
2024-04-26 23:51:08 +0530

    Update 3d-card-snippet.tsx

--- app/snippets/3d-card-snippet.tsx
+++ app/snippets/3d-card-snippet.tsx
@@ -23,8 +23,7 @@ export function ThreeDCardExample() {
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <Image
-            // src="images/sexy-ui.png"
-            src=""
+            src="images/sexy-ui.png"
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
